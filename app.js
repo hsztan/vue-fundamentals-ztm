9884275a5b63d76cab2caeabda21a278a8d2572e
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-09-03 23:10:48 -0500

    v-on directive

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const vm = Vue.createApp({
       firstName: 'John',
       lastName: 'Doe',
       url: 'https://www.google.com',
+      raw_url: '<a href="https://www.google.com" target="_blank">Google</a>',
+      age: 20,
     };
   },
   methods: {
     fullName() {
       return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
     },
   },
 }).mount('#app');
